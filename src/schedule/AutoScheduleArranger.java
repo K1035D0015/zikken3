70232875af52f530ea1e9120f3dbda2270125155
baseball <baseball@t-pc>
2017-01-13 16:17:47 +0900

    ab

--- src/schedule/AutoScheduleArranger.java
+++ src/schedule/AutoScheduleArranger.java
@@ -5,23 +5,53 @@
  */
 package schedule;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
 /**
  *
  * @author baseball
  */
 public class AutoScheduleArranger {
-    public boolean Priority(){
+    private Object a;
+    List = new ArrayList;
+    
+    //スケジュール取得関数
+    void acquisition(){
+        //各方面から送られたスケジュールを習得する
+                
+    }       
+    
+    //データベーススケジュール取得関数
+    void getSchedule(){
+        //データベースからスケジュールを取得する
+        
+    }
+    
+    //優先順位比較関数
+    boolean Priority(){
+        //2つのスケジュールを比較して、優先順位の高いものを採用する。
+        if( a.compare(i,j) == 0)
+            return false;
         return false;       
     }
+    
+    //スケジュール追加関数
     public boolean addSchedule(){
+        //ScheduleDatebaseHandlerのadd関数を呼ぶ
         return false;        
     }
     
+    //進捗更新関数
     public boolean updateProgress(){
+        //ScheduleDatebaseHandlerのupdate関数を呼ぶ
         return false;        
     }
     
+    //スケジュール削除関数
     public boolean deteteSchedule(){
+        //ScheduleDatebaseHandlerのdelete関数を呼ぶ
         return false;       
     }
     
