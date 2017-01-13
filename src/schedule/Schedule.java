aa740a52053386f62562b06f716ef45e63a9d935
rmaloid <>
2017-01-13 14:58:20 +0900

    Scheduleクラスを追加しました。

---
+++ src/schedule/Schedule.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package schedule;
+
+import java.util.Calendar;
+
+/**
+ *
+ * @author o231056s
+ */
+public class Schedule {
+    
+    public boolean isEvent; //TrueならEvent、FalseならTask
+    public Calendar startTimeB; //開始時間(ユーザーが設定した時間)
+    public Calendar 
+    
+}
